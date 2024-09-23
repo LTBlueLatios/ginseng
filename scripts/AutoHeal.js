e18456b2c10f4a0a7d56ee074f538233f3ba7d7a
BlueLatios <65719723+LTBlueLatios@users.noreply.github.com>
2024-09-23 02:39:37 +0000

    Minor fix

--- scripts/AutoHeal.js
+++ scripts/AutoHeal.js
@@ -24,19 +24,19 @@ const AutoHeal = {
 
         const playerTick = game.ui.getPlayerTick();
         if (this.config.healPlayer) {
-            healEntity(playerTick, "HealthPotion", this.config.playerThreshold, 'player');
+            this.healEntity(playerTick, "HealthPotion", this.config.playerThreshold, 'player');
         }
         
         const petTick = game.ui.getPlayerPetTick();
         if (this.config.healPet) {
-            healEntity(petTick, "PetHealthPotion", this.config.petThreshold, 'pet');
+            this.healEntity(petTick, "PetHealthPotion", this.config.petThreshold, 'pet');
         }
     },
     healEntity(entityTick, itemName, threshold, canHealKey) {
         if (!entityTick) return;
         const { dead, health, maxHealth } = entityTick;
     
-        if (!dead && calculateHealthPercentage(health, maxHealth) <= threshold) {
+        if (!dead && this.calculateHealthPercentage(health, maxHealth) <= threshold) {
             game.network.sendRpc({ name: "BuyItem", itemName, tier: 1 });
             game.network.sendRpc({ name: "EquipItem", itemName, tier: 1 });
     
